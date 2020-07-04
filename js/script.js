1258af1b1f01c5291849ef423440d96c1fbe2d9f
Nikrosh <nikrosh91@gmail.com>
2020-07-04 23:34:26 +0300

    Поправил замечания

--- js/script.js
+++ js/script.js
@@ -15,98 +15,98 @@ var creditSection = document.querySelector('.service-credit');
 var sliderButtons = document.querySelectorAll('.button-head-slider');
 var headSlides = document.querySelectorAll('.head-slide');
 
-if (body.classList.contains("body-index")) {
+if (body.classList.contains('body-index')) {
 
 // Modal-contact
 
-modContactButton.addEventListener("click", function (evt) {
+modContactButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modContainer.classList.add("flex-show");
-  modContact.classList.add("block-show");
+  modContainer.classList.add('flex-show');
+  modContact.classList.add('block-show');
 });
 
-modContactClose.addEventListener("click", function (evt) {
+modContactClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modContainer.classList.remove("flex-show");
-  modContact.classList.remove("block-show");
+  modContainer.classList.remove('flex-show');
+  modContact.classList.remove('block-show');
 });
 
 // Modal-map
 
-modMapButton.addEventListener("click", function (evt) {
+modMapButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modContainer.classList.add("flex-show");
-  modMap.classList.add("block-show");
+  modContainer.classList.add('flex-show');
+  modMap.classList.add('block-show');
 });
 
-modMapClose.addEventListener("click", function (evt) {
+modMapClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modContainer.classList.remove("flex-show");
-  modMap.classList.remove("block-show");
+  modContainer.classList.remove('flex-show');
+  modMap.classList.remove('block-show');
 });
 
 // Services
 
-deliveryButton.addEventListener("click", function (evt) {
+deliveryButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  deliverySection.classList.add("block-show");
-  deliveryButton.classList.add("services-button-checked");
-  guaranteeSection.classList.remove("block-show");
-  guaranteeButton.classList.remove("services-button-checked");
-  creditSection.classList.remove("block-show");
-  creditButton.classList.remove("services-button-checked");
+  deliverySection.classList.add('block-show');
+  deliveryButton.classList.add('services-button-checked');
+  guaranteeSection.classList.remove('block-show');
+  guaranteeButton.classList.remove('services-button-checked');
+  creditSection.classList.remove('block-show');
+  creditButton.classList.remove('services-button-checked');
 });
 
-guaranteeButton.addEventListener("click", function (evt) {
+guaranteeButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  deliverySection.classList.remove("block-show");
-  deliveryButton.classList.remove("services-button-checked");
-  guaranteeSection.classList.add("block-show");
-  guaranteeButton.classList.add("services-button-checked");
-  creditSection.classList.remove("block-show");
-  creditButton.classList.remove("services-button-checked");
+  deliverySection.classList.remove('block-show');
+  deliveryButton.classList.remove('services-button-checked');
+  guaranteeSection.classList.add('block-show');
+  guaranteeButton.classList.add('services-button-checked');
+  creditSection.classList.remove('block-show');
+  creditButton.classList.remove('services-button-checked');
 });
 
-creditButton.addEventListener("click", function (evt) {
+creditButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  deliverySection.classList.remove("block-show");
-  deliveryButton.classList.remove("services-button-checked");
-  guaranteeSection.classList.remove("block-show");
-  guaranteeButton.classList.remove("services-button-checked");
-  creditSection.classList.add("block-show");
-  creditButton.classList.add("services-button-checked");
+  deliverySection.classList.remove('block-show');
+  deliveryButton.classList.remove('services-button-checked');
+  guaranteeSection.classList.remove('block-show');
+  guaranteeButton.classList.remove('services-button-checked');
+  creditSection.classList.add('block-show');
+  creditButton.classList.add('services-button-checked');
 });
 
 // slider
 
-sliderButtons[0].addEventListener("click", function (evt) {
+sliderButtons[0].addEventListener('click', function (evt) {
   evt.preventDefault();
-  sliderButtons[0].classList.add("button-slider-checked");
-  sliderButtons[1].classList.remove("button-slider-checked");
-  sliderButtons[2].classList.remove("button-slider-checked");
-  headSlides[0].classList.add("flex-show");
-  headSlides[1].classList.remove("flex-show");
-  headSlides[2].classList.remove("flex-show");
+  sliderButtons[0].classList.add('button-slider-checked');
+  sliderButtons[1].classList.remove('button-slider-checked');
+  sliderButtons[2].classList.remove('button-slider-checked');
+  headSlides[0].classList.add('flex-show');
+  headSlides[1].classList.remove('flex-show');
+  headSlides[2].classList.remove('flex-show');
 });
 
-sliderButtons[1].addEventListener("click", function (evt) {
+sliderButtons[1].addEventListener('click', function (evt) {
   evt.preventDefault();
-  sliderButtons[0].classList.remove("button-slider-checked");
-  sliderButtons[1].classList.add("button-slider-checked");
-  sliderButtons[2].classList.remove("button-slider-checked");
-  headSlides[0].classList.remove("flex-show");
-  headSlides[1].classList.add("flex-show");
-  headSlides[2].classList.remove("flex-show");
+  sliderButtons[0].classList.remove('button-slider-checked');
+  sliderButtons[1].classList.add('button-slider-checked');
+  sliderButtons[2].classList.remove('button-slider-checked');
+  headSlides[0].classList.remove('flex-show');
+  headSlides[1].classList.add('flex-show');
+  headSlides[2].classList.remove('flex-show');
 });
 
-sliderButtons[2].addEventListener("click", function (evt) {
+sliderButtons[2].addEventListener('click', function (evt) {
   evt.preventDefault();
-  sliderButtons[0].classList.remove("button-slider-checked");
-  sliderButtons[1].classList.remove("button-slider-checked");
-  sliderButtons[2].classList.add("button-slider-checked");
-  headSlides[0].classList.remove("flex-show");
-  headSlides[1].classList.remove("flex-show");
-  headSlides[2].classList.add("flex-show");
+  sliderButtons[0].classList.remove('button-slider-checked');
+  sliderButtons[1].classList.remove('button-slider-checked');
+  sliderButtons[2].classList.add('button-slider-checked');
+  headSlides[0].classList.remove('flex-show');
+  headSlides[1].classList.remove('flex-show');
+  headSlides[2].classList.add('flex-show');
 });
 
 };
